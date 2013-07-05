dc7e1ce96d50344cdcb711f55072a14dfddccd70
uce <u.celebi@fu-berlin.de>
2013-07-05 12:32:59 +0200

    GradientJob: allow to set initial weight vector

--- aim3-logreg/src/test/java/com/celebihacker/ml/logreg/GradientJobTest.java
+++ aim3-logreg/src/test/java/com/celebihacker/ml/logreg/GradientJobTest.java
@@ -9,22 +9,24 @@ public class GradientJobTest {
 
   public static final boolean RUN_LOCAL_MODE = true;
   
-  static final String INPUT_FILE_TRAIN_LOCAL = "/home/andre/dev/datasets/RCV1-v2/vectors/lyrl2004_vectors_train.seq";
-  static final String INPUT_FILE_TRAIN_HDFS = "rcv1-v2/lyrl2004_vectors_train.seq";
+  static final String INPUT_FILE_TRAIN_LOCAL = "/Users/uce/Desktop/rcv1-v2/vectors/lyrl2004_vectors_train_5000.seq";
+  static final String INPUT_FILE_TRAIN_HDFS ="hdfs://localhost:9000/user/uce/rcv1/rcv1-v2/lyrl2004_vectors_train_5000.seq";
   
-  static final String OUTPUT_TRAIN_PATH = "output-aim3-ensemble";
+  static final String OUTPUT_TRAIN_PATH = "output-aim3-gradient";
   
   static final String JAR_PATH = "target/aim3-logreg-0.0.1-SNAPSHOT-job.jar";
   static final String CONFIG_FILE_PATH = "core-site.xml";
 
   @Test
   public void test() throws Exception {
-    ToolRunner.run(new GradientJob(
+    GradientJob job = new GradientJob(
         INPUT_FILE_TRAIN_LOCAL,
-        INPUT_FILE_TRAIN_HDFS, 
-        OUTPUT_TRAIN_PATH, 
-        JAR_PATH, 
+        INPUT_FILE_TRAIN_HDFS,
+        OUTPUT_TRAIN_PATH,
+        JAR_PATH,
         CONFIG_FILE_PATH,
-        RUN_LOCAL_MODE), null);
+        RUN_LOCAL_MODE);    
+
+    ToolRunner.run(job, null);
   }
 }
